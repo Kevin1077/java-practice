1a1d43630dd98ee54ddb1d4d9c56943d35e2d79e
Kevin Biju Kulangara <118618402+Kevin1077@users.noreply.github.com>
2023-11-19 12:51:01 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of rows:");
+		int row=sc.nextInt();
+		
+		for(int  i=0;i<row;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("  ");
+			}
+			for(int k=1;k<=row-i;k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
